be10285dbcde953488fa74db487f1217b9c3ab30
BinojBI <binojishara21@gmail.com>
2018-05-31 20:34:21 +0530

    37th commit

--- profile.php
+++ profile.php
@@ -1,1 +1,40 @@
-<?php echo "this is profile page"; ?>
+<?php ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Your Profile</title>
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="js/bootstrap.min.js">
+    <link rel="stylesheet" href="profstyle.css" type="text/css">
+    <link rel="stylesheet" href="/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.css">
+<script src="js/jquery-3.3.1.min.js"></script>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="logo">
+            <img src="images/logo3.png" alt="logo">
+          </div>
+        </div>
+        <div class="col-md-4">
+          <header>
+            <h2>Hi, Welcome to your profile..!</h2>
+          </header>
+        </div>
+        <div class="col-md-4">
+          <div class="right-corner">
+            <h5><a href="index.php">Back to << Home Page</a>
+            </h5>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
